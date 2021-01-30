8e688377b102ba075baf947e8a88144581799eca
ex1list <a.michail@list.ru>
2021-01-30 12:08:31 +0300

    Second aproximation  1.0 with Factory Pattern

--- test.php
+++ test.php
@@ -1,98 +1,155 @@
 <?php
 
-    class FactoryRobot {
-        private $robot_types = []; 
-        // method for added robot type 
-        public function addType ($newRobots) {
-            $this->robot_types[] = $newRobots;
-            //var_dump($this->robot_types);  
-            return $this->robot_types;
-        }    
-        public function createRobot ($count_copies, $Robot) {
-            $robot_copies = []; 
-            for ($i = 1; $i <= $count_copies; $i++) {
-                $robot_copies[]=$Robot;
-            } 
-            //return array of objects
-            return $robot_copies;
-        }        
+    interface Robots {
+        public function getVelocity(): float;
+        public function getWeight(): float;
+        public function getHeight(): float;
+    }
+
+    class RobotProperties implements Robots {
+        public $velocity;
+        public $height;
+        public $weight;
+        public function __construct(float $velocity, float $weight, float $height) {
+            $this->velocity=$velocity;
+            $this->weight = $weight;
+            $this->height = $height;     
+        }
+        public function getVelocity(): float {
+            return $this->velocity;
+        }
+        public function getWeight(): float {
+            return $this->weight;
+        }
+        public function getHeight(): float {
+            return $this->height;
+        }
     }
     
-    // class union of robots 
-    
     class  MergeRobot {  
-        public $merge_weight = 0;
-        public $merge_height = 0;
-        public $merge_velocity = 0;   
-        public function addRobot  ($Robot) {  
-            $this->merge_weight+= $Robot->weight;
-            $this->merge_height+= $Robot->height;
-            if ($this->merge_velocity == 0 ) {
-                $this->merge_velocity = $Robot->velocity;
-            } elseif ($Robot->velocity < $this->merge_velocity) {
-                $this->merge_velocity = $Robot->velocity;  
-            } 
+        public $merge_weight = null;
+        public $merge_height = null;
+        public $merge_velocity = null;   
+        
+        public function addRobot  ($Robot) {
+            // var_dump($Robot); die();
+            foreach ($Robot as $InstanceRobot) { 
+                // var_dump($InstanceRobot); die();
+                $this->merge_weight+= $InstanceRobot->weight;
+                $this->merge_height+= $InstanceRobot->height;
+                if ($this->merge_velocity == 0 ) {
+                    $this->merge_velocity = $InstanceRobot->velocity;
+                } elseif ($InstanceRobot->velocity < $this->merge_velocity) {
+                     $this->merge_velocity = $InstanceRobot->velocity;  
+                    } 
+            }
         }
+        
         public function getWeight() {  
             return "merge_weight: ".$this->merge_weight." ";
-        }   
+        } 
+        
         public function getHeight() {  
             return "merge_height: ".$this->merge_height." ";
         } 
-        public function getSpeed() {  
-            return "merge_Speed: ".$this->merge_velocity." ";
-        }                
-    }          
         
-    // create first robot  
-    class Robot1 {
-        public $weight = null;
-        public $height = null;
-        public $velocity = null;     
-        function __construct($w,$h,$v) {
-            $this-> weight = $w;
-            $this->height = $h;
-            $this->velocity = $v;     
-        }    
+        public function getSpeed() {  
+           return "merge_Speed: ".$this->merge_velocity." ";
+        }      
     }
-    // create second robot    
-    class Robot2 {
-        public $weight = null;
-        public $height = null;
-        public $velocity = null;
-        function __construct($w,$h,$v) {
-            $this-> weight = $w;
-            $this->height = $h;
-            $this->velocity = $v;     
-        }     
-    }   
-    // Instance of class factory     
-    $factory = new FactoryRobot;
-    $R1 = $factory->addType(new Robot1(1,2,3)) ;
-    // var_dump($R1); die();
-    $R2 = $factory->addType(new Robot2(2,1,0)) ;
-    // var_dump($R2); die();
-    // cloning object of robot
-    $clone = $factory->createRobot(5,$R1);
-    // var_dump($clone);die();
-    $mergeRobot = new MergeRobot();
+        
+    class FactoryRobot {
+        
+        public static function addType ($velocity,$weight, $height): RobotProperties {
+            return new RobotProperties ($velocity,$weight, $height);
+        }
+            
+        public static function createRobot1 ($countcopies,$Robot1) {
+            // var_dump($Robot1->velocity); die();
+            $robot_copies = []; 
+            if ($countcopies == 1) {
+                 return $Robot1;
+            } else {
+                for ($i = 1; $i <= $countcopies; $i++) {             
+                    $Robot1->velocity +=rand(1, 5);
+                    $Robot1->weight +=rand(1, 5);
+                    $Robot1->height +=rand(1, 5);           
+                    $robot_copies[]=$Robot1;
+                    // var_dump($robot_copies);
+                }  
+            }
+            //return array of objects
+            return $robot_copies;
+        }
+        
+        public static function createRobot2 ($countcopies,$Robot2) {
+            // var_dump($Robot2->velocity); die();
+            $robot_copies = []; 
+            if ($countcopies ==1) {
+                 return $Robot2;
+            } else {
+                for ($i = 1; $i <= $countcopies; $i++) {             
+                    $Robot2->velocity +=rand(1,5);
+                    $Robot2->weight +=rand(1,5);
+                    $Robot2->height +=rand(1,5);           
+                    $robot_copies[]=$Robot2;
+                    var_dump($robot_copies);
+                }  
+            }
+            //return array of objects
+            return $robot_copies;
+        }      
+        
+    }  
+           
+    //Добавления типа Robot1 роботов которые создает фабрика
+    
+    $Robot1 = FactoryRobot::addType (500,100, 200);
+    echo 'Velocity: ' . $Robot1->getVelocity();
+    echo 'Height: ' . $Robot1->getWeight();
+    echo 'Weight: ' . $Robot1->getHeight();
+    
+    // Массив из 5 объектов разных роботов первого типа
+    
+    $ArrayRobot1 = FactoryRobot::createRobot1 (5,$Robot1);
+    
+    //Добавления типа Robot2 роботов которые создает фабрика
+    
+    $Robot2 = FactoryRobot::addType (250,111, 320);
+    echo 'Velocity: '. $Robot2->getVelocity();
+    echo 'Height: ' . $Robot2->getWeight();
+    echo 'Weight: ' . $Robot2->getHeight();
+    
+    // Массив из 2 объектов разных роботов второго типа
+    
+    $ArrayRobot2 = FactoryRobot::createRobot2 (2,$Robot2); 
+    
+    // Объединение роботов в один
+    
+    $mergeRobot1 = new MergeRobot();
+    
     // start union different robots without cloning
-    $mergeRobot->addRobot(new Robot2(2,1,23));
-    $mergeRobot->addRobot(new Robot2(3,8,11));
-    $mergeRobot->addRobot(new Robot2(4,1,15));
-    $mergeRobot->addRobot(new Robot2(5,2,120));
-    $mergeRobot->addRobot(new Robot2(4,3,25));
+    
+    $mergeRobot1->addRobot($Robot2); 
+    
     //minimum speed of all robots 
+    
     echo $mergeRobot->getSpeed();
+    
     //the sum of all masses of robots 
+    
     echo $mergeRobot->getWeight();
+    
     //the sum of all masses of robots 
+    
     echo $mergeRobot->getHeight();
+    
     // added new type robot
-    $factory->addType($mergeRobot );
-    // add to union robots create with clonning
-    //var_dump($mergeRobot->addRobot($factory->createRobot(5,$R2)));
+    
+    $factory->addType($mergeRobot1);
+    
     // array pointer to its first element 
-    //$res = reset($factory->createMergeRobot(1));
-
-?>
+    
+    $res = reset($factory->createMergeRobot(1));
+    
+    //echo $res->getSpeed();
