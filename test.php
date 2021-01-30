a0d686d4af9b8209bc6078c0aab690aa5b89313d
ex1list <a.michail@list.ru>
2021-01-30 12:38:48 +0300

    Second approximation with Factory pattern fix

--- test.php
+++ test.php
@@ -32,15 +32,15 @@
         public $merge_velocity = null;   
         
         public function addRobot  ($Robot) {
-            // var_dump($Robot); die();
-            foreach ($Robot as $InstanceRobot) { 
-                // var_dump($InstanceRobot); die();
-                $this->merge_weight+= $InstanceRobot->weight;
-                $this->merge_height+= $InstanceRobot->height;
+             //var_dump($Robot); die();
+            foreach ($Robot as $InstanceThisTypeRobot) { 
+                  //var_dump($InstanceThisTypeRobot); die();
+                $this->merge_weight+= $InstanceThisTypeRobot['weight'];
+                $this->merge_height+= $InstanceThisTypeRobot['height'];
                 if ($this->merge_velocity == 0 ) {
-                    $this->merge_velocity = $InstanceRobot->velocity;
-                } elseif ($InstanceRobot->velocity < $this->merge_velocity) {
-                     $this->merge_velocity = $InstanceRobot->velocity;  
+                    $this->merge_velocity = $InstanceThisTypeRobot['velocity'];
+                } elseif ($InstanceThisTypeRobot['velocity'] < $this->merge_velocity) {
+                     $this->merge_velocity = $InstanceThisTypeRobot['velocity'];  
                     } 
             }
         }
@@ -70,11 +70,11 @@
             if ($countcopies == 1) {
                  return $Robot1;
             } else {
-                for ($i = 1; $i <= $countcopies; $i++) {             
+                for ($i = 1; $i <= $countcopies; $i++) { 
+                    $robot_copies[]=get_object_vars($Robot1);
                     $Robot1->velocity +=rand(1, 5);
                     $Robot1->weight +=rand(1, 5);
-                    $Robot1->height +=rand(1, 5);           
-                    $robot_copies[]=$Robot1;
+                    $Robot1->height +=rand(1, 5);              
                     // var_dump($robot_copies);
                 }  
             }
@@ -88,13 +88,14 @@
             if ($countcopies ==1) {
                  return $Robot2;
             } else {
-                for ($i = 1; $i <= $countcopies; $i++) {             
+                for ($i = 1; $i <= $countcopies; $i++) {              
+                    $robot_copies[]= get_object_vars($Robot2);
                     $Robot2->velocity +=rand(1,5);
                     $Robot2->weight +=rand(1,5);
                     $Robot2->height +=rand(1,5);           
-                    $robot_copies[]=$Robot2;
-                    var_dump($robot_copies);
-                }  
+                    
+                }   
+                 //var_dump($robot_copies); die();
             }
             //return array of objects
             return $robot_copies;
@@ -103,53 +104,53 @@
     }  
            
     //Добавления типа Robot1 роботов которые создает фабрика
-    
+    echo "Robot1"."<BR>";
     $Robot1 = FactoryRobot::addType (500,100, 200);
-    echo 'Velocity: ' . $Robot1->getVelocity();
-    echo 'Height: ' . $Robot1->getWeight();
-    echo 'Weight: ' . $Robot1->getHeight();
+    echo 'Velocity: ' . $Robot1->getVelocity()."<BR>";
+    echo 'Height: ' . $Robot1->getWeight()."<BR>";
+    echo 'Weight: ' . $Robot1->getHeight()."<BR>"."<BR>"."<BR>";
     
     // Массив из 5 объектов разных роботов первого типа
-    
+    echo "Массив из 5 объектов разных роботов первого типа:"."<BR>";
     $ArrayRobot1 = FactoryRobot::createRobot1 (5,$Robot1);
-    
+    var_dump($ArrayRobot1);
     //Добавления типа Robot2 роботов которые создает фабрика
-    
+    echo "Robot2"."<BR>";
     $Robot2 = FactoryRobot::addType (250,111, 320);
-    echo 'Velocity: '. $Robot2->getVelocity();
-    echo 'Height: ' . $Robot2->getWeight();
-    echo 'Weight: ' . $Robot2->getHeight();
+    echo 'Velocity: '. $Robot2->getVelocity()."<BR>";
+    echo 'Height: ' . $Robot2->getWeight()."<BR>";
+    echo 'Weight: ' . $Robot2->getHeight()."<BR>"."<BR>"."<BR>";
     
     // Массив из 2 объектов разных роботов второго типа
-    
+    echo "Массив из 2 объектов разных роботов второго типа:"."<BR>";
     $ArrayRobot2 = FactoryRobot::createRobot2 (2,$Robot2); 
-    
+    var_dump($ArrayRobot2);
     // Объединение роботов в один
     
     $mergeRobot1 = new MergeRobot();
     
     // start union different robots without cloning
-    
-    $mergeRobot1->addRobot($Robot2); 
+    echo "Объединение 2 объектов разных роботов второго типа:"."<BR>";
+    $mergeRobot1->addRobot($ArrayRobot2); 
     
     //minimum speed of all robots 
-    
-    echo $mergeRobot->getSpeed();
-    
-    //the sum of all masses of robots 
-    
-    echo $mergeRobot->getWeight();
+    echo "Минимальная скорость:";
+    echo $mergeRobot1->getSpeed()."<BR>";
     
     //the sum of all masses of robots 
+    echo "Суммарная масса:";
+    echo $mergeRobot1->getWeight()."<BR>";
     
-    echo $mergeRobot->getHeight();
+    //the sum of all masses of robots 
+    echo "Минимальная высота:";
+    echo $mergeRobot1->getHeight()."<BR>";
     
     // added new type robot
     
-    $factory->addType($mergeRobot1);
+    //$factory->addType($mergeRobot1);
     
     // array pointer to its first element 
     
-    $res = reset($factory->createMergeRobot(1));
+    //$res = reset($factory->createMergeRobot(1));
     
     //echo $res->getSpeed();
